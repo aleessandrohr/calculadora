c9788a66bb74528d55480bb49bf5f7756649c9c7
Alessandro Ramos <toprime13@gmail.com>
2020-11-17 19:37:28 -0300

    update 1.3

--- código/javascript/script.js
+++ código/javascript/script.js
@@ -5,47 +5,47 @@ function somar() {
     if (n1.value.length == 0 | n2.value.length == 0) {
         alert('Preencha todos os dados.')
     } else {
-        var n1n = Number(n1.value)
-        var n2n = Number(n2.value)
-        var s = n1n + n2n
+        let n1n = Number(n1.value)
+        let n2n = Number(n2.value)
+        let s = n1n + n2n
         alert(`O resultado é ${s}.`)
     }
 
 }
+
 function subtrair() {
     if (n1.value.length == 0 | n2.value.length == 0) {
         alert('Preencha todos os dados.')
     } else {
-    var n1n = Number(n1.value)
-    var n2n = Number(n2.value)
-    var s = n1n - n2n
+    let n1n = Number(n1.value)
+    let n2n = Number(n2.value)
+    let s = n1n - n2n
     alert(`O resultado é ${s}.`)
     }
 }
+
 function multiplicar() {
     if (n1.value.length == 0 | n2.value.length == 0) {
         alert('Preencha todos os dados.')
     } else {
-    var n1n = Number(n1.value)
-    var n2n = Number(n2.value)
-    var s = n1n * n2n
+    let n1n = Number(n1.value)
+    let n2n = Number(n2.value)
+    let s = n1n * n2n
     alert(`O resultado é ${s}.`)
     }
 }
+
 function dividir() {
     if (n1.value.length == 0 | n2.value.length == 0) {
         alert('Preencha todos os dados.')
     } else {
-    var n1n = Number(n1.value)
-    var n2n = Number(n2.value)
-    var s = n1n / n2n
+    let n1n = Number(n1.value)
+    let n2n = Number(n2.value)
+    let s = n1n / n2n
     alert(`O resultado é ${s}.`)
     }
 }
 
-function ver(){
-    alert('Versão 1.2')
-    alert('Melhoria de código e novo layout.')
-    alert('Se quiser me ajuda a melhorar mais ainda o código e o layout :D')
+function ver() {
+    alert('Versão 1.\n\Melhoria de código e novo layout.')
 }
-
